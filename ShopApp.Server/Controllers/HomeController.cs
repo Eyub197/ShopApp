8b80971ea34b243e1781b231d4d3580ac4cd48fb
Ekrem05 <ekremnptg@gmail.com>
2024-03-07 20:03:07 +0200

    StartUp

---
+++ ShopApp.Server/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopApp.Server.Data;
+using ShopApp.Server.Models;
+
+namespace ShopApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private ShopAppDbContext context;
+
+        public HomeController(ShopAppDbContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpGet(Name ="Index")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<ICollection<Product>>> Index()
+        {
+            var products=await context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+    }
+}
---
+++ ShopApp.Server/Migrations/20240307180130_Initial.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopApp.Server.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Image", "Name", "Price" },
+                values: new object[] { 1, "Description 1", "https://via.placeholder.com/150", "Product 1", 100m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Image", "Name", "Price" },
+                values: new object[] { 2, "Description 2", "https://via.placeholder.com/150", "Product 2", 200m });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Image", "Name", "Price" },
+                values: new object[] { 3, "Description 3", "https://via.placeholder.com/150", "Product 3", 300m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
