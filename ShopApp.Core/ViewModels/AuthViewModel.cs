087bd65af8136d2edf200182b250605cdeab1197
Ekrem05 <ekremnptg@gmail.com>
2024-03-27 18:58:04 +0200

    Log in works correctly

---
+++ ShopApp.Core/ViewModels/AuthViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopApp.Core.ViewModels
+{
+    public class AuthViewModel
+    {
+        public string Token { get; set; } = string.Empty;   
+
+        public bool Result { get; set; }
+
+        public List<string> Errors{ get; set; }=new List<string>();
+    }
+}
